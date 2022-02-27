e4d6857cfa206a91e0bda4d51a26c8cf45c2dacd
Joeway Banh <joewaybanh@hotmail.com>
2022-02-27 16:54:53 +0100

    initial commit after finishing the test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css'],
+
+  template: `
+    <body>
+      <button
+        [className]="clicked ? 'liked like-button' : 'like-button'"
+        (click)="toggle()"
+      >
+        Like |
+        <span>{{ initialCount }}</span>
+      </button>
+    </body>
+  `,
 })
 export class AppComponent {
-  title = 'like-button';
+  public initialCount: number = 100;
+  public clicked: boolean = false;
+
+  toggle = () => {
+    this.clicked = !this.clicked;
+
+    if (this.clicked) {
+      this.initialCount = 101;
+    } else {
+      this.initialCount = 100;
+    }
+  };
 }
